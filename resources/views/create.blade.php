5061f6d2e76ec0ab111a55d763b1801c51349355
dinarreskifauzi <luxxygoodboy@gmail.com>
2026-02-01 19:07:30 +0700

    test

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -47,7 +47,7 @@
     <label for="exampleInputPassword1" class="form-label">Keterangan</label>
     <input type="text" name="keterangan" class="form-control" id="exampleInputPassword1">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Tabung</button>
 </form>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
--- resources/views/tarik.blade.php
+++ resources/views/tarik.blade.php
@@ -43,7 +43,7 @@
     <label for="exampleInputPassword1" class="form-label">Tanggal</label>
     <input type="date" name="tanggal" class="form-control" id="exampleInputPassword1">
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-danger">Tarik</button>
 </form>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
